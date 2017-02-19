167ba0502976efe6b5e1bde5c4bba2b9f3ef8ff1
Robert Butts <robert.o.butts@gmail.com>
2017-02-19 11:31:24 -0700

    Add Session genserver

--- lib/exedra.ex
+++ lib/exedra.ex
@@ -1,17 +1,23 @@
 defmodule Exedra do
   use Application
+  alias Exedra.User, as: User
+  alias Exedra.Room, as: Room
+  alias Exedra.Item, as: Item
+  alias Exedra.SSHManager, as: SSHManager
+  alias Exedra.SessionManager, as: SessionManager
 
   @spec start(any, any) :: Supervisor.on_start
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      worker(Exedra.SSHManager, [])
+      worker(SSHManager, []),
+      worker(SessionManager, [SessionManager])
     ]
 
-    :ok = Exedra.User.load()
-    :ok = Exedra.Room.load()
-    :ok = Exedra.Item.load()
+    :ok = User.load()
+    :ok = Room.load()
+    :ok = Item.load()
 
     opts = [strategy: :one_for_one, name: Exedra.Supervisor]
     Supervisor.start_link(children, opts)
---
+++ lib/session.ex
@@ -0,0 +1,52 @@
+defmodule Exedra.SessionManager do
+  use GenServer
+
+  defmodule Data do
+    @enforce_keys [:user, :pid]
+    defstruct user: "", pid: 0
+  end
+
+  @spec get(GenServer.server, String.t) :: {:reply, {:ok, Data} | :error, %{}}
+  def get(server, user) do
+    GenServer.call server, {:get, user}
+  end
+
+  @spec set(GenServer.server, String.t, pid) :: :ok
+  def set(server, user, pid) do
+    GenServer.cast server, {:set, user, pid}
+  end
+
+  @spec delete(GenServer.server, String.t) :: :ok
+  def delete(server, user) do
+    GenServer.cast server, {:delete, user}
+  end
+
+
+  @spec init({String.t, integer}) :: {:ok, %{}}
+  def init([]) do
+    {:ok,  %{}}
+  end
+
+  @spec start_link(String.t) :: GenServer.on_start
+  def start_link(name) do
+    GenServer.start_link __MODULE__, [], name: name
+  end
+
+  @spec handle_call({:get, String.t}, any, %{}) :: {:reply, {:ok, pid} | :error, %{}}
+  def handle_call({:get, user}, _from, data) do
+    reply = Map.fetch(data, user)
+    {:reply, reply, data}
+  end
+
+  @spec handle_cast({:set, String.t, pid}, %{}) :: {:noreply, %{}}
+  def handle_cast({:set, user, pid}, data) do
+    data = Map.put data, user, pid
+    {:noreply, data}
+  end
+
+  @spec handle_cast({:delete, String.t}, %{}) :: {:noreply, %{}}
+  def handle_cast({:delete, user}, data) do
+    data = Map.delete(data, user)
+    {:noreply, data}
+  end
+end
